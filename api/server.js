daf9ac157ca1daf9596c01673180fa25f6a951c8
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-09-21 10:22:04 -0700

    added server.js

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const usersRouter = require("../users/usersRouter");
+const authRouter = require("../auth/authRouter");
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+server.use("/api", usersRouter);
+
+server.use("/api/auth", authRouter);
+
+module.exports = server;
