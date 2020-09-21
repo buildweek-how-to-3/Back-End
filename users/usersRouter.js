30478b21349a96be6c6611bb2117a07c2601bca2
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-09-21 10:24:08 -0700

    userRouter.js is created for getting all the list of users

---
+++ users/usersRouter.js
@@ -0,0 +1,17 @@
+const restricted = require("../auth/restrictedMiddleware");
+const router = require("express").Router();
+
+const Users = require("./users-model");
+
+router.get("/users", restricted, (req, res) => {
+  Users.get()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.status(500).json({ message: error.message });
+    });
+});
+
+module.exports = router;
