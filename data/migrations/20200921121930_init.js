d1570b23b27ed6c2eb7e0d05344fe71bba669718
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-09-22 14:46:49 -0700

    updated migrations and seed data

--- data/migrations/20200921121930_init.js
+++ data/migrations/20200921121930_init.js
@@ -23,5 +23,5 @@ exports.up = function (knex) {
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("users").dropTableIfExists("posts");
+  return knex.schema.dropTableIfExists("posts").dropTableIfExists("users");
 };
